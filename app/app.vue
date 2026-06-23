dbd5c6ffcbe94eb3d915ed289a24762b3f9e3be7
LyheangDev <lyheangdev@gmail.com>
2026-06-23 13:56:20 +0700

    deploy:v3

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,7 @@
     <nav class="w-full flex items-center justify-between">
       <div class="bg-gray-900/5 dark:bg-gray-50/10 py-2.5 pl-3.5 pr-20 rounded-xl">
         <h4 class="text-gray-800 font-bold dark:text-white">SVI HR <span class="text-base font-normal">Management</span></h4>
-        <p class="text-xs dark:text-gray-300 font-light leading-snug">Fast, modern and reliable</p>
+        <p class="text-xs dark:text-gray-300 font-light leading-snug">Fast, modern and reliable v 1.0.0</p>
       </div>
       <button class="py-1 px-2 rounded-lg cursor-pointer bg-amber-500/15 dark:bg-amber-500/10 flex items-center gap-2" @click="handleToggleTheme">
         <IconLight v-if="themeMode=='light'" class="stroke-amber-500 dark:stroke-white text-transparent size-6!" />
