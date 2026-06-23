e0421711ca7c5d0bd97db24e1edfed29d7f3a3dc
LyheangDev <lyheangdev@gmail.com>
2026-06-23 22:20:56 +0700

    add system menu

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,11 @@
 <template>
   <main data-theme="light"
     class="w-screen h-full bg-white dark:bg-black py-2 px-2 transition-colors duration-300 ease-out">
-
     <nav
       class="flex flex-col pt-5 px-4 bg-bg-secondary border border-white/5 w-[240px] max-w-[240px] rounded-xl h-[100dvh]">
       <!-- Brand Logo -->
       <section>
-        <div class="flex items-center gap-3">
+        <div class="flex items-center gap-3 pb-5">
           <IconSVI class="text-blue-900 h-8! w-10!" />
           <div>
             <h5 class="text-gray-800 text-sm font-medium dark:text-white leading-snug">SVI HR System</h5>
@@ -16,15 +15,38 @@
       </section>
 
       <!-- Feature Section -->
-      <section class="py-8">
-        <h5 class="text-white text-xs font-semibold">Overview</h5>
+      <section class="pt-5">
+        <h5 class="text-gray-400 uppercase text-xs font-semibold">Overview</h5>
         <ul class="py-3 flex flex-col gap-1">
-          <li v-for="m in menu" :key="m.name" class="cursor-pointer w-full rounded-lg py-2 px-3 flex items-center gap-2.5 hover:bg-white/10 duration-300 ease-out"
-            :class="{'bg-white/5' : m.route.toLowerCase() == route.path.toLowerCase()}"
-          >
-            <component :is="m.icon" class="size-5! text-white" />
-            <span class="text-xs font-normal text-gray-200 leading-snug">{{ capitalize(m.name) }}</span>
-          </li>
+          <NuxtLink v-for="m in featureMenu" :key="m.name" :href="m.route">
+            <li
+              class="cursor-pointer w-full rounded-lg py-2 px-3 flex items-center gap-2.5 hover:bg-white/10 duration-300 ease-out"
+              :class="{ 'bg-white/5': m.route.toLowerCase() == route.path.toLowerCase() }">
+              <component :is="m.icon" class="size-5! text-white"
+                :class="{ 'text-blue-700!': m.route.toLowerCase() == route.path.toLowerCase() }" />
+              <span class="text-xs font-normal text-gray-200 leading-snug"
+                :class="{ 'font-semibold': m.route.toLowerCase() == route.path.toLowerCase() }">{{ capitalize(m.name)
+                }}</span>
+            </li>
+          </NuxtLink>
+        </ul>
+      </section>
+
+      <!-- System Section -->
+      <section class="pt-5">
+        <h5 class="text-gray-400 uppercase text-xs font-semibold">System Management</h5>
+        <ul class="py-3 flex flex-col gap-1">
+          <NuxtLink v-for="m in systemMenu" :key="m.name" :href="m.route">
+            <li
+              class="cursor-pointer w-full rounded-lg py-2 px-3 flex items-center gap-2.5 hover:bg-white/10 duration-300 ease-out"
+              :class="{ 'bg-white/5': m.route.toLowerCase() == route.path.toLowerCase() }">
+              <component :is="m.icon" class="size-5! text-white"
+                :class="{ 'text-blue-700!': m.route.toLowerCase() == route.path.toLowerCase() }" />
+              <span class="text-xs font-normal text-gray-200 leading-snug"
+                :class="{ 'font-semibold': m.route.toLowerCase() == route.path.toLowerCase() }">{{ capitalize(m.name)
+                }}</span>
+            </li>
+          </NuxtLink>
         </ul>
       </section>
     </nav>
@@ -45,6 +67,10 @@ import IconHome from '~/assets/icons/home.svg';
 import IconEmployee from '~/assets/icons/multi-user.svg';
 import IconAttendance from '~/assets/icons/attendance.svg';
 import IconDoc from '~/assets/icons/doc.svg';
+import IconUser from '~/assets/icons/user.svg';
+import IconKeys from '~/assets/icons/keys.svg';
+import IconSetting from '~/assets/icons/setting-config.svg';
+
 import { capitalize } from 'vue';
 
 type ThemeMode = 'light' | 'dark';
@@ -52,7 +78,7 @@ const themeMode = ref<ThemeMode>('light');
 
 const route = useRoute();
 
-const menu = [{
+const featureMenu = [{
   name: "Dashboard",
   icon: IconHome,
   route: '/',
@@ -67,10 +93,27 @@ const menu = [{
   icon: IconAttendance,
   route: '/attendance',
   children: []
-},{
+}, {
   name: "Leave Request",
   icon: IconDoc,
   route: '/leave-request',
+  children: []
+}]
+
+const systemMenu = [{
+  name: "User Control",
+  icon: IconUser,
+  route: '/user-control',
+  children: []
+}, {
+  name: "Role & Permission",
+  icon: IconKeys,
+  route: '/role-permission',
+  children: []
+}, {
+  name: "System Configuration",
+  icon: IconSetting,
+  route: '/configuration',
   children: []
 }]
 
