3ab4db828233364f892e6f917265b8d066fc7d82
LyheangDev <lyheangdev@gmail.com>
2026-06-23 21:47:29 +0700

    config menu

--- app/app.vue
+++ app/app.vue
@@ -1,20 +1,39 @@
 <template>
-  <main data-theme="light" class="px-3 py-3 w-screen h-screen bg-white dark:bg-bg-primary transition-colors duration-300 ease-out">
-    
-    <nav class="w-full flex items-center justify-between">
-      <div class="bg-gray-900/5 dark:bg-gray-50/10 py-2.5 pl-4 pr-6 rounded-lg flex items-center gap-3">
-        <IconSVI class="text-blue-900 h-10! w-12!" />
-        <div>
-          <h5 class="text-gray-800 font-bold dark:text-white leading-snug">SVI Dashboard</h5>
-          <p class="text-xs dark:text-gray-300 font-light leading-snug">Fast, modern</p>
+  <main data-theme="light"
+    class="w-screen h-full bg-white dark:bg-black py-2 px-2 transition-colors duration-300 ease-out">
+
+    <nav
+      class="flex flex-col pt-5 px-4 bg-bg-secondary border border-white/5 w-[240px] max-w-[240px] rounded-xl h-[100dvh]">
+      <!-- Brand Logo -->
+      <section>
+        <div class="flex items-center gap-3">
+          <IconSVI class="text-blue-900 h-8! w-10!" />
+          <div>
+            <h5 class="text-gray-800 text-sm font-medium dark:text-white leading-snug">SVI HR System</h5>
+            <p class="text-[10px] dark:text-gray-300 font-light leading-snug">Fast, modern</p>
+          </div>
         </div>
-      </div>
-      <button class="py-1 px-2 rounded-lg cursor-pointer bg-amber-500/15 dark:bg-amber-500/10 flex items-center gap-2" @click="handleToggleTheme">
+      </section>
+
+      <!-- Feature Section -->
+      <section class="py-8">
+        <h5 class="text-white text-xs font-semibold">Overview</h5>
+        <ul class="py-3 flex flex-col gap-1">
+          <li v-for="m in menu" :key="m.name" class="cursor-pointer w-full rounded-lg py-2 px-3 flex items-center gap-2.5 hover:bg-white/10 duration-300 ease-out"
+            :class="{'bg-white/5' : m.route.toLowerCase() == route.path.toLowerCase()}"
+          >
+            <component :is="m.icon" class="size-5! text-white" />
+            <span class="text-xs font-normal text-gray-200 leading-snug">{{ capitalize(m.name) }}</span>
+          </li>
+        </ul>
+      </section>
+    </nav>
+
+    <!-- <button class="py-1 px-2 rounded-lg cursor-pointer bg-amber-500/15 dark:bg-amber-500/10 flex items-center gap-2" @click="handleToggleTheme">
         <IconLight v-if="themeMode=='light'" class="stroke-amber-500 dark:stroke-white text-transparent size-6!" />
         <IconDark v-else class="size-6! text-white" />
         <span class="text-xs font-medium dark:text-amber-500">{{ themeMode === 'light' ? 'Light Mode' : 'Dark Mode' }}</span>
-      </button>
-    </nav>
+      </button> -->
   </main>
 </template>
 
@@ -22,9 +41,38 @@
 import IconLight from '~/assets/icons/light.svg';
 import IconDark from '~/assets/icons/dark.svg';
 import IconSVI from '~/assets/icons/logo.svg';
+import IconHome from '~/assets/icons/home.svg';
+import IconEmployee from '~/assets/icons/multi-user.svg';
+import IconAttendance from '~/assets/icons/attendance.svg';
+import IconDoc from '~/assets/icons/doc.svg';
+import { capitalize } from 'vue';
 
 type ThemeMode = 'light' | 'dark';
 const themeMode = ref<ThemeMode>('light');
+
+const route = useRoute();
+
+const menu = [{
+  name: "Dashboard",
+  icon: IconHome,
+  route: '/',
+  children: []
+}, {
+  name: "Employees",
+  icon: IconEmployee,
+  route: '/employees',
+  children: []
+}, {
+  name: "Attendance",
+  icon: IconAttendance,
+  route: '/attendance',
+  children: []
+},{
+  name: "Leave Request",
+  icon: IconDoc,
+  route: '/leave-request',
+  children: []
+}]
 
 onMounted(() => {
   //-- Load the OS theme preference
@@ -33,14 +81,14 @@ onMounted(() => {
   themeMode.value = osMode;
 })
 
-watch(themeMode , (mode) => {
-  if(typeof window !== 'undefined'){
+watch(themeMode, (mode) => {
+  if (typeof window !== 'undefined') {
     localStorage.setItem('themeMode', mode);
     const main = document.querySelector('main');
-    if(!main) return;
-    main.setAttribute('data-theme',themeMode.value);
+    if (!main) return;
+    main.setAttribute('data-theme', themeMode.value);
   }
-},{immediate: true})
+}, { immediate: true })
 
 function handleToggleTheme() {
   themeMode.value = themeMode.value === 'light' ? 'dark' : 'light';
