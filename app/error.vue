c1fa600e5d84a3e9682fd05b5b36b4fe21931fd0
LyheangDev <lyheangdev@gmail.com>
2026-06-30 13:36:07 +0700

    add fallback page

---
+++ app/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLayout name="default">
+    <div class="flex flex-col justify-center items-center h-full m-auto">
+      <div class="p-2.5 bg-black/5 dark:bg-white/10 rounded-full w-fit">
+        <IconBuild class="size-10! text-gray-800 dark:text-gray-100" />
+      </div>
+      <h4 class="text-gray-800 dark:text-gray-100 font-semibold leading-loose">
+        Feature is under develop
+      </h4>
+      <p class="text-gray-400 text-sm mt-1">Upcomming realease</p>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import IconBuild from '~/assets/icons/build.svg';
+
+definePageMeta({
+  layout: 'default',
+});
+</script>
---
+++ app/pages/dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pt-5">
+    <h4 class="text-gray-100">Welcome SVI Dashboard</h4>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default',
+});
+</script>
